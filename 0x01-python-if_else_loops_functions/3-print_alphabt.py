1d19b61650d517c278a18456e60034cd7a2b447e
NALE <n_wanyama@yahoo.com>
2023-03-07 12:55:38 +0000

    Modification print alphabet skipping some

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -3,4 +3,4 @@ for i in range(ord('a'), ord('z') + 1):
     if i == ord('q') or i == ord('e'):
         pass
     else:
-        print(f"{chr(i)}", end="")
+        print("{}".format(chr(i)), end="")
