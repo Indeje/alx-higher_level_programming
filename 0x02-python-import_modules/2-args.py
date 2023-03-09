f25f7b5db188cd6f85ba0e4318417f704e862f9b
Nale <n_wanyama@yahoo.com>
2023-03-09 15:19:27 +0300

    Modified 2-args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,9 @@ import sys
 
 if __name__ == "__main__":
     count = 1
-    if len(sys.argv) == 2:
+    if len(sys.argv) == 1:
+        print(f"{len(sys.argv) - 1} arguments.")
+    elif len(sys.argv) == 2:
         print(f"{len(sys.argv) - 1} argument:")
     else:
         print(f"{len(sys.argv) - 1} arguments:")
