7b469e5f9c5833d261b9cdcce625fc29064faa95
Nale <n_wanyama@yahoo.com>
2023-03-09 15:01:30 +0300

    Print CLI arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    count = 1
+    if len(sys.argv) == 2:
+        print(f"{len(sys.argv) - 1} argument:")
+    else:
+        print(f"{len(sys.argv) - 1} arguments:")
+    args = sys.argv
+    args.pop(0)
+    for arg in args:
+        print(f"{count}: {arg}")
+        count += 1
