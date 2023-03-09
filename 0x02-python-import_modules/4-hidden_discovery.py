0db5923a910a96318dba0c6a73c10f929c9fb2bf
Nale <n_wanyama@yahoo.com>
2023-03-09 16:19:37 +0300

    Hidden discovery

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
-import hidden_4
+if __name__ == '__main__':
+    import hidden_4
 
+    name_list = dir(hidden_4)
+    for st in name_list:
+        if not st.startswith('__'):
+            print('{:s}'.format(st))
