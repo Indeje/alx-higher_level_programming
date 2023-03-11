d582d202cc17d53ea0965cec60a520ead3f1c32c
Nale <n_wanyama@yahoo.com>
2023-03-11 17:42:47 +0300

    Quickly swap values in two variables

--- 0x03-python-data_structures/main.py
+++ 0x03-python-data_structures/main.py
@@ -98,10 +98,11 @@ while i < len(list_result):
     i += 1 """
 
 # 11-main
-delete_at = __import__('11-delete_at').delete_at
+""" delete_at = __import__('11-delete_at').delete_at
 
 my_list = [1, 2, 3, 4, 5]
 idx = 3
 new_list = delete_at(my_list, idx)
 print(new_list)
-print(my_list)
+print(my_list) """
+
