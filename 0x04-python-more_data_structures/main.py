9ede712e9fb25ed62ec131cf273f0cf64f2dc368
Nale <n_wanyama@yahoo.com>
2023-03-15 14:12:57 +0300

    Add unique integers in a list and return the sum

--- 0x04-python-more_data_structures/main.py
+++ 0x04-python-more_data_structures/main.py
@@ -14,10 +14,17 @@ print(new_matrix)
 print(matrix) """
 
 # 1-main
-search_replace = __import__('1-search_replace').search_replace
+""" search_replace = __import__('1-search_replace').search_replace
 
 my_list = [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89]
 new_list = search_replace(my_list, 2, 89)
 
 print(new_list)
-print(my_list)
+print(my_list) """
+
+# 2-main
+uniq_add = __import__('2-uniq_add').uniq_add
+
+my_list = [1, 2, 3, 1, 4, 2, 5]
+result = uniq_add(my_list)
+print("Result: {:d}".format(result))
