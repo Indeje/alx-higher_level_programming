1e3ce88c367535012ad9c0f8f34c121595296425
Nale <n_wanyama@yahoo.com>
2023-03-17 12:29:02 +0300

    Multiply dictionary values by 2 and return new dictionary

--- 0x04-python-more_data_structures/main.py
+++ 0x04-python-more_data_structures/main.py
@@ -77,7 +77,7 @@ print("--")
 print_sorted_dictionary(a_dictionary) """
 
 # 8-main
-simple_delete = __import__('8-simple_delete').simple_delete
+""" simple_delete = __import__('8-simple_delete').simple_delete
 print_sorted_dictionary = \
     __import__('6-print_sorted_dictionary').print_sorted_dictionary
 
@@ -90,6 +90,17 @@ print_sorted_dictionary(new_dict)
 print("--")
 print("--")
 new_dict = simple_delete(a_dictionary, 'c_is_fun')
+print_sorted_dictionary(a_dictionary)
+print("--")
+print_sorted_dictionary(new_dict) """
+
+# 9-main
+multiply_by_2 = __import__('9-multiply_by_2').multiply_by_2
+print_sorted_dictionary = \
+    __import__('6-print_sorted_dictionary').print_sorted_dictionary
+
+a_dictionary = {'John': 12, 'Alex': 8, 'Bob': 14, 'Mike': 14, 'Molly': 16}
+new_dict = multiply_by_2(a_dictionary)
 print_sorted_dictionary(a_dictionary)
 print("--")
 print_sorted_dictionary(new_dict)
