80e72ac9c9f17f7e2e91173dc03564a2fd4c15f1
Nale <n_wanyama@yahoo.com>
2023-03-22 14:41:02 +0300

    Err line too long

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return list(map(lambda nested_list: 
-                    list(map(lambda x: x * x, nested_list)), matrix))
+    return list(map(lambda y: list(map(lambda x: x * x, y)), matrix))
