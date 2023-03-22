3f39edc2b9e8349f26dbc931f27279a54778005e
Nale <n_wanyama@yahoo.com>
2023-03-22 14:38:11 +0300

    Removed for loop in 101-square_matrix_map.py

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return [list(map(lambda x: x * x, nested_list)) for nested_list in matrix]
+    return list(map(lambda nested_list: list(map(lambda x: x * x, nested_list)), matrix))
