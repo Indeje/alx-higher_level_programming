049607d6863004c12912d7046d402dbe2aada8ff
Nale <n_wanyama@yahoo.com>
2023-03-29 15:15:40 +0300

    Raise TyperError exception

--- 0x05-python-exceptions/main.py
+++ 0x05-python-exceptions/main.py
@@ -60,7 +60,7 @@ result = safe_print_division(a, b)
 print("{:d} / {:d} = {}".format(a, b, result)) """
 
 # 4-main
-list_division = __import__('4-list_division').list_division
+""" list_division = __import__('4-list_division').list_division
 
 my_l_1 = [10, 8, 4]
 my_l_2 = [2, 4, 4]
@@ -72,4 +72,12 @@ print("--")
 my_l_1 = [10, 8, 4, 4]
 my_l_2 = [2, 0, "H", 2, 7]
 result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
-print(result)
+print(result) """
+
+# 5-main
+raise_exception = __import__('5-raise_exception').raise_exception
+
+try:
+    raise_exception()
+except TypeError as te:
+    print("Exception raised")
