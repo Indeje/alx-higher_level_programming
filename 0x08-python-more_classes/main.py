1f715506083b1669b40282b4c2c0a378b91095bb
Nale <n_wanyama@yahoo.com>
2023-04-06 12:42:27 +0300

    Custom __repr__ method

--- 0x08-python-more_classes/main.py
+++ 0x08-python-more_classes/main.py
@@ -25,7 +25,7 @@ print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perime
 
 
 # 3-main
-Rectangle = __import__('3-rectangle').Rectangle
+""" Rectangle = __import__('3-rectangle').Rectangle
 
 my_rectangle = Rectangle(2, 4)
 print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
@@ -38,4 +38,32 @@ print("--")
 my_rectangle.width = 10
 my_rectangle.height = 3
 print(my_rectangle)
+print(repr(my_rectangle)) """
+
+
+# 4-main
+Rectangle = __import__('4-rectangle').Rectangle
+
+my_rectangle = Rectangle(2, 4)
+print(str(my_rectangle))
+print("--")
+print(my_rectangle)
+print("--")
 print(repr(my_rectangle))
+print("--")
+print(hex(id(my_rectangle)))
+print("--")
+
+# create new instance based on representation
+new_rectangle = eval(repr(my_rectangle))
+print(str(new_rectangle))
+print("--")
+print(new_rectangle)
+print("--")
+print(repr(new_rectangle))
+print("--")
+print(hex(id(new_rectangle)))
+print("--")
+
+print(new_rectangle is my_rectangle)
+print(type(new_rectangle) is type(my_rectangle))
--- 0x08-python-more_classes/tests.py
+++ 0x08-python-more_classes/tests.py
@@ -1,92 +1,10 @@
 #!/usr/bin/python3
 
-"""
-    Module with rectangle class
-"""
+class Rect:
+    ...
 
+x = 'print(5)'
+eval(x)
 
-class Rectangle:
-    """
-        A class that defines a rectangle
-    """
-
-    def __init__(self, width=0, height=0) -> None:
-        """
-            Checks the parameters and initializes values
-            Args:
-                width (:obj:`int`, optional): The width of the Rectangle.
-                height (:obj:`int`, optional): The height of the Rectangle.
-        """
-
-        if self.__check_width(width) and self.__check_height(height):
-            self.__width = width
-            self.__height = height
-
-    @property
-    def width(self):
-        """
-            Width property getter
-        """
-
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        """
-            Width property setter
-        """
-
-        self.__check_width(value)
-        self.__width = value
-
-    @property
-    def height(self):
-        """
-            Height property getter
-        """
-
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        """
-            Height property setter
-        """
-
-        self.__check_height(value)
-        self.__height = value
-
-    def __check_width(self, width):
-        """
-            Check width parameter
-            Args:
-                Width (int) - Rectangle width
-        """
-
-        if type(width) is not int:
-            raise TypeError('width must be an integer')
-        if width < 0:
-            raise ValueError('width must be >= 0')
-        return True
-
-    def __check_height(self, height):
-        """
-            Check height parameter
-            Args:
-                Height (int) - Rectangle height
-        """
-
-        if type(height) is not int:
-            raise TypeError('height must be an integer')
-        if height < 0:
-            raise ValueError('height must be >= 0')
-        return True
-
-
-if __name__ == '__main__':
-    my_rectangle = Rectangle(2, 4)
-    print(my_rectangle.__dict__)
-
-    my_rectangle.width = 10
-    my_rectangle.height = 3
-    print(my_rectangle.__dict__)
+my_rect = Rect()
+print(type(my_rect))
