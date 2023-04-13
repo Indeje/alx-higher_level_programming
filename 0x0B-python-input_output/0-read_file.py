981c066c23e71bb5a9e56883fdacaa2d2c31b00c
Nale <n_wanyama@yahoo.com>
2023-04-13 04:19:45 +0300

    Read text file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
 
+""" Read file """
+
+
 def read_file(filename=''):
+
+    """
+        Function that reads text file
+    """
+
     with open(filename, encoding='utf8') as newfile:
         for line in newfile:
             print(line, end='')
