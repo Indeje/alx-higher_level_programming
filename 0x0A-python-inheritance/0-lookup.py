f846736506adc1a9349d150e7d5b464d07c5dae5
Nale <n_wanyama@yahoo.com>
2023-05-08 17:41:26 +0300

    Pycodestyle kinda sucks

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
 
 """
-	Module that contains 'lookup' function
+Module that contains 'lookup' function
 """
+
 
 def lookup(obj):
 
+
 	"""
-		Function that finds available attributes and methods of an object
-		Args:
-			obj: Object
-		Returns:
-			list of object attributes and methods
+	Function that finds available attributes and methods of an object
+	Args:
+	obj: Object
+	Returns:
+	list of object attributes and methods
 	"""
 
 	return dir(obj)
