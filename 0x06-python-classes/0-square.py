3ade2d51302532aac3630504aede775b25e1c966
Nale <n_wanyama@yahoo.com>
2023-07-07 14:39:38 +0300

    Module Documentation

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 
+
+"""
+    Module defines a square
+"""
+
+
 class Square:
 
     """ Defines a square """
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+"""
+    Module defines a square
+"""
+
+
+class Square:
+
+    """ Defines a square """
